0c556ea73651c34e3b85c9ed13868a4649753a7f
devairjr <devair@ymail.com>
2016-11-03 17:42:03 -0200

    Aula 2.13 - refactoring

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -27,22 +27,6 @@ public class LivrosResources {
 	@Autowired
 	private LivrosService livrosServices;
 	
-	// diz qual a URI que será usada para acessar o método, por meio do método GET do HTTP
-	/*@RequestMapping(value = "/livros", method = RequestMethod.GET)
-	public List<Livro> listar(){
-		
-		Exemplo de retorno de uma lista de objetos
-		
-		Livro l1 = new Livro("Rest Aplicado");
-		Livro l2 = new Livro("Git passo-a-passo");
-		
-		Livro[] livros = {l1,l2};
-		
-		return Arrays.asList(livros);
-	}*/
-	
-	//Método que busca dados do banco de dados
-	//@RequestMapping(value = "/livros", method = RequestMethod.GET)
 	@RequestMapping(method = RequestMethod.GET)//Quando é usado a anotation @RequestMapping não é necessário passar o value
 	public ResponseEntity<List<Livro>>listar(){
 		return ResponseEntity.status(HttpStatus.OK).body(livrosServices.lista());
@@ -62,46 +46,20 @@ public class LivrosResources {
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)//Vai realizar um GET e na URI livros/id o Spring pega o id e realiza a consulta
 	public ResponseEntity<?> buscar(@PathVariable("id") Long id){
-		
-		/*
-		 * return sem tratamento de no_data_found
-		 * return livrosRepository.findOne(id);
-		 */
-		
-		Livro livro = null;
-		
-		try{
-			livro = livrosServices.buscar(id);
-		}catch (LivroNaoEncontradoException e){
-			return ResponseEntity.notFound().build();
-	    }
-		
+		Livro livro = livrosServices.buscar(id);
 		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> deletar(@PathVariable("id") Long id){
-		
-		try{
-			livrosServices.deletar(id);
-		}catch(LivroNaoEncontradoException e){
-			return ResponseEntity.notFound().build();
-		}
-		
+		livrosServices.deletar(id);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable("id") Long id){
-		
 		livro.setId(id);
-		
-		try {
-			livrosServices.atualizar(livro);
-		} catch (LivroNaoEncontradoException e) {
-			return ResponseEntity.notFound().build();
-		}
-		
+		livrosServices.atualizar(livro);
 		return ResponseEntity.noContent().build();
 	}
 }
